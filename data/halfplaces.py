0ee940f1ca3340757adf396b97a36ae5c6fed6d9
JMGamse <31906340+JMGamse@users.noreply.github.com>
2017-10-27 13:33:23 -0400

    Add files via upload
    first half of places

---
+++ data/halfplaces.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 27 12:42:12 2017
+
+@author: joega
+"""
+
+places = [
+    {
+        "name": "manhattan",
+        "properName": "Manhattan",
+        "boundingBox": [
+            -74.026675,
+            40.683935,
+            -73.910408,
+            40.877483
+        ],
+        "weatherStationID": "725030-14732"
+    },
+    {
+        "name": "sanFrancisco",
+        "properName": "San Francisco",
+        "boundingBox": [
+            -122.514926,
+            37.708075,
+            -122.357031,
+            37.833238
+        ],
+        "weatherStationID": "994016-99999"
+    },
+    {
+        "name": "washingtonDC",
+        "properName": "Washington DC",
+        "boundingBox": [
+            -77.119401,
+            38.801826,
+            -76.909396,
+            38.9953797
+        ],
+        "weatherStationID": "997314-99999"
+    },
+    {
+        "name": "houston",
+        "properName": "Houston",
+        "boundingBox": [
+            -95.823268,
+            29.522325,
+            -95.069705,
+            30.1546646
+        ],
+        "weatherStationID": "720594-00188"
+    },
+    {
+        "name": "denver",
+        "properName": "Denver",
+        "boundingBox": [
+            -105.109815,
+            39.614151,
+            -104.734372,
+            39.812975
+        ],
+        "weatherStationID": "999999-23012"
+    },
+    {
+        "name": "seattle",
+        "properName": "Seattle",
+        "boundingBox": [
+            -122.436232,
+            47.4953154,
+            -122.2249728,
+            47.734319
+        ],
+        "weatherStationID": "994014-99999"
+    },
+    {
+        "name": "chicago",
+        "properName": "Chicago",
+        "boundingBox": [
+            -87.940033,
+            41.644102,
+            -87.523993,
+            42.0230669
+        ],
+        "weatherStationID": "725346-94866"
+    },
+    {
+        "name": "boston",
+        "properName": "Boston",
+        "boundingBox": [
+            -71.191421,
+            42.227797,
+            -70.986004,
+            42.399542
+        ],
+        "weatherStationID": "994971-99999"
+    },
+]
