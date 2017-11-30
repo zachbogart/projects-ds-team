82e401eb6cceb561bd272f15554da83547524580
ramyij <31906137+ramyij@users.noreply.github.com>
2017-11-30 12:00:00 -0500

    added function to handle missing data
    function handles empty data in single file.

--- scripts/enrich/weather/getNewWeather.py
+++ scripts/enrich/weather/getNewWeather.py
@@ -22,3 +22,46 @@ def getWeatherForCoordinates(gps):
         hourlyWeather[day] = (hourly)
 
     return hourlyWeather
+
+def handleNansInDict(json_file):
+    data_file = open(json_file)
+    json_data = json.load(data_file)
+    
+#     nans = df[df.isnull().any(axis=1)]
+#     print "Here are the columns containing Nans :"
+#     print pd.isnull(nans).sum() > 0
+    
+    for i in range(len(json_data)):
+        d = json_data[i]
+
+    # precipType: set value to 'NoPrecip'
+        if 'precipType' not in d.keys():
+            d['precipType'] = 'NoPrecip'
+        
+    # remove ozone, windGusts and uv index from the dataframe
+        try:
+            del d['ozone']
+        except KeyError:
+            pass
+        try:
+            del d['uvIndex']
+        except KeyError:
+            pass
+        try:
+            del d['windGust']
+        except KeyError:
+            pass
+        
+    #handle cloudCover
+        iconDict = {'fog': .5,'clear-night': 0,'clear-day': 0, 'partly-cloudy-day': .5, 'cloudy': .8, u'partly-cloudy-night': .5, 'wind': .5, 'rain': .7}
+    
+        if 'cloudCover' not in d.keys():
+            d['cloudCover'] = iconDict[d['icon']]
+    
+    json_data[i] = d
+    
+    outputPath = json_file[:-5] + 'er.json'
+    with open(outputPath, 'w') as outfile:
+        json.dump(json_data, outfile)
+
+    
